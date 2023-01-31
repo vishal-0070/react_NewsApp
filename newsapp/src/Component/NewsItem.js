73970c2906a02d5cc8d058b4e15dad9744a166d9
vishalmangla141222@gmail.com <vishalmangla141222@gmail.com>
2023-01-31 23:48:50 +0530

    NewsItem update commit

--- newsapp/src/Component/NewsItem.js
+++ newsapp/src/Component/NewsItem.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react'
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    let{title,description}=this.props;
+const NewsItem =(props)=> {
+
+  
+    let {title,description,imageUrl,newsUrl,author,date,source}=props;
     return (
-      <div>
-        <div className='card' style={{width:"18rem"}}>
-            <img scr="..." className='card-img-top' alt="..."/>
-            <div className='card-body'>
-                <h5 className=' card-title'>{title}</h5>
-                <p className='card-text'>{description}</p>
-                <a href="/" className='btn btn-primary'>Go somewhere</a>
-            </div>
+      <div className="my-3">
+        <div className="card">
+        <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }>
+
+                        <span className="badge rounded-pill bg-danger"> {source} </span>
+                    </div>
+          <img src={!imageUrl?"https://img.etimg.com/thumb/msid-95426900,width-1070,height-580,imgsize-26312,overlay-etmarkets/photo.jpg":imageUrl }className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description} </p>
+            <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+            <a rel=" noopener noreferrer" href={newsUrl} target="_blank" className="btn btn sm btn-dark">Read More </a>
+          </div>
         </div>
       </div>
-    )
-  }
+    );
+  
 }
 
-export default NewsItem
+export default NewsItem;

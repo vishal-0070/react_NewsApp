20b5591eecd4b9617703d1b092d07069045d8deb
vishalmangla141222@gmail.com <vishalmangla141222@gmail.com>
2023-02-01 18:56:55 +0530

    commit

--- newsapp/src/Component/Signup/Signup.js
+++ newsapp/src/Component/Signup/Signup.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+
+import InputControl from "../InputControl/InputControl";
+import { auth } from "../../firebase";
+
+import styles from "./Signup.module.css";
 
 function Signup() {
   return (
